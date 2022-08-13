73d70c002d15f6299bcd1610dc191f866924b78b
uelionobre <uelionobre@hotmail.com>
2022-08-13 13:03:37 -0300

    formata mensagens de cada passo da execucao

--- fundamentos/bloco-01-unix-bash-e-shell-script/parte-1.sh
+++ fundamentos/bloco-01-unix-bash-e-shell-script/parte-1.sh
@@ -8,12 +8,12 @@ mkdir unix_tests
 sleep $long_delay
 
 # Exercício 1
-echo "Exercício 1 - Acessar o diretório unix_tests"
+echo "Acessa o diretório unix_tests"
 cd unix_tests
 sleep $long_delay
 
 # Exercício 2
-echo "Exercicio 2 - Adiciona Skills ao arquivo skills.txt"
+echo "Adiciona Skills ao arquivo skills.txt"
 touch skills2.txt
 echo 'Internet' >> skills2.txt
 echo 'Unix' >> skills2.txt
@@ -21,7 +21,7 @@ echo 'Bash' >> skills2.txt
 sleep $long_delay
 
 # Exercício 3
-echo "Exercício 3 - Adiciona mais 5 itens a lista de skills"
+echo "Adiciona mais 5 itens a lista de skills"
 echo "React" >> skills2.txt
 echo "Redux" >> skills2.txt
 echo "Node" >> skills2.txt
