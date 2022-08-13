1fd0acc73c709b4551668235946b525412051146
uelionobre <uelionobre@hotmail.com>
2022-08-13 18:12:48 -0300

    parte 2 - exercicio 3

--- fundamentos/bloco-01-unix-bash-e-shell-script/dia-04-unix-e-bash-part-2/README.md
+++ fundamentos/bloco-01-unix-bash-e-shell-script/dia-04-unix-e-bash-part-2/README.md
@@ -31,3 +31,5 @@
 1. Navegue até a pasta unix_tests;
 
 2. Rode o comando ls -l e veja quais as permissões dos arquivos;
+
+3. Mude a permissão do arquivo `bunch_of_things.txt` para que todos os usuários possam ter acesso à leitura e escrita, e verifique se está correto com o comando `ls -l`;
--- fundamentos/bloco-01-unix-bash-e-shell-script/dia-04-unix-e-bash-part-2/parte-2.sh
+++ fundamentos/bloco-01-unix-bash-e-shell-script/dia-04-unix-e-bash-part-2/parte-2.sh
@@ -7,3 +7,8 @@ cd unix_tests
 # Exercicio 2
 echo "2 - Roda o comando ls para ver permissões dos arquivos"
 echo "$(ls -la)"
+
+# Exercicio 3
+echo "3 - Mude a permissão do arquivo bunch_of_things.txt para leitura e escrita para todos os usuario e confere"
+chmod 666 bunch_of_things.txt
+echo "$(ls -l)"
