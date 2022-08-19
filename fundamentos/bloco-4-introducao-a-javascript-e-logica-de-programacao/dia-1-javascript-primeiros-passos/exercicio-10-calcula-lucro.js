ead5614334d699aa578b1ed99fb9843b68d189ca
uelionobre <uelionobre@hotmail.com>
2022-08-19 08:56:17 -0300

    Resolve exercicio 10

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-10-calcula-lucro.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-10-calcula-lucro.js
@@ -1,18 +1,21 @@
-const valorCusto = 1.3;
-const valorVenda = 2.5;
+const valorCusto = 1;
+const valorVenda = 2;
 
 if (valorCusto < 0 || valorVenda < 0) {
   console.log("Erro: Valores inválidos");
 } else {
-  let quantidade = 1000;
+  let quantidade = 10;
+  let impostoCusto = valorCusto * 1.2;
 
-  let totalCusto = valorCusto * quantidade;
+  let totalCusto = impostoCusto * quantidade;
   let totalVenda = valorVenda * quantidade;
+
   let lucro = totalVenda - totalCusto;
 
   let msgCusto = "Valor total de Custo: " + totalCusto.toFixed(2);
   let msgVenda = "Valor total de Venda: " + totalVenda.toFixed(2);
   let msgLucro = "Lucro de: " + lucro.toFixed(2);
+  let msgImposto = "Imposto de: " + impostoCusto.toFixed(2);
 
-  console.log(`${msgCusto}\n${msgVenda}\n${msgLucro}`);
+  console.log(`${msgCusto}\n${msgVenda}\n${msgLucro}\n${msgImposto}`);
 }
