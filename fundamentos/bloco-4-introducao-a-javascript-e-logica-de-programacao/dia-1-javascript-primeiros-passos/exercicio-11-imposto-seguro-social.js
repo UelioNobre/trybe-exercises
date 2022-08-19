0e5bb3e5ba881dcf6d6b944e5b9457c3f520175c
uelionobre <uelionobre@hotmail.com>
2022-08-19 09:10:36 -0300

    adiciona logica 8pct

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-11-imposto-seguro-social.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-11-imposto-seguro-social.js
@@ -1,4 +1,14 @@
-const salarioBruto = 0;
+const salarioBruto = 1556;
+
+let impostoINSS = 0;
+
+if (salarioBruto <= 1556.94) {
+  console.log("8 %");
+} else {
+  console.log("9 %");
+  console.log("11 %");
+  console.log("570.88");
+}
 
 const salarioLiquido = (1 + 1).toFixed(2);
-console.log("salarioLiquido", salarioLiquido);
+// console.log("salarioLiquido", salarioLiquido);
