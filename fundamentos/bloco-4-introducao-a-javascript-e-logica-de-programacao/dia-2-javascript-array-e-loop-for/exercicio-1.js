71c757acca33baff729f95ddb881b5a6f4bacc62
uelionobre <uelionobre@hotmail.com>
2022-08-19 13:40:14 -0300

    soma numeros

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1.js
@@ -4,3 +4,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let number of numbers) {
   console.log(number);
 }
+
+// Exercicio 2
+let soma = 0;
+for (let number of numbers) {
+  soma += number;
+}
+console.log("soma: ", soma);
