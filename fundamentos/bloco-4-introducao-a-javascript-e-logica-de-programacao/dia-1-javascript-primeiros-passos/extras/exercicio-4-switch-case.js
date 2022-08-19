9d04386cce1c311f29f224d047be1ac067a86672
uelionobre <uelionobre@hotmail.com>
2022-08-19 00:28:30 -0300

    adiciona exercicio 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/extras/exercicio-4-switch-case.js
@@ -0,0 +1,15 @@
+let status;
+
+switch (status) {
+  case "reprovada":
+    console.log("Você foi reprovada(o)");
+    break;
+  case "lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+  case "aprovada":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+  default:
+    console.log("Não se aplica");
+}
