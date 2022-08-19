b399a58deb061aff3832c8ffaeb3cee2d099bf14
uelionobre <uelionobre@hotmail.com>
2022-08-19 00:02:41 -0300

    adiciona constante e base

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/extras/exercicio-2-tipagem-e-operacoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/extras/exercicio-2-tipagem-e-operacoes.js
@@ -5,3 +5,13 @@ const patientInfo = {
   lastName: "Santos",
 };
 const patientEmail = "ana@email.com";
+
+//
+// console.log(typeof patientAge);
+// console.log(typeof patientId);
+
+// patientId = "50";
+// console.log(typeof patientId);
+
+const base = 5;
+let height = 8;
