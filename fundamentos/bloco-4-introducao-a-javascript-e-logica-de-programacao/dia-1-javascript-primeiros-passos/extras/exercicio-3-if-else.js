042972abe4a700bc483b78ef122a88e411d91275
uelionobre <uelionobre@hotmail.com>
2022-08-19 00:18:40 -0300

    implementa requisito 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/extras/exercicio-3-if-else.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/extras/exercicio-3-if-else.js
@@ -1,13 +1,9 @@
 const candidatePerson = 79;
 
-if (candidatePerson >= 80) {
-  console.log("Parabéns, você foi aprovada(o)!");
-}
-
-if (candidatePerson >= 60 && candidatePerson < 80) {
-  console.log("Você está na nossa lista de espera");
-}
-
 if (candidatePerson < 60) {
   console.log("Você foi reprovada(o)");
+} else if (candidatePerson >= 60 && candidatePerson < 80) {
+  console.log("Você está na nossa lista de espera");
+} else {
+  console.log("Parabéns, você foi aprovada(o)!");
 }
