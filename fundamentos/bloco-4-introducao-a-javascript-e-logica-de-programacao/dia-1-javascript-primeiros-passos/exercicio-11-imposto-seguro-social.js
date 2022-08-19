9225cceaff4a1cad5d0825a737e384645e61f293
uelionobre <uelionobre@hotmail.com>
2022-08-19 10:10:03 -0300

    calcula salario liquido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-11-imposto-seguro-social.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-11-imposto-seguro-social.js
@@ -1,4 +1,4 @@
-const salarioBruto = 5189.81;
+let salarioBruto = 4000;
 
 let calculoINSS = 0;
 
@@ -12,7 +12,40 @@ if (salarioBruto <= 1556.94) {
   calculoINSS = 570.88;
 }
 
-let impostoINSS = calculoINSS.toFixed(2);
+let impostoINSS = calculoINSS;
 
+// Calculo IR
+
+let salarioIR = salarioBruto - impostoINSS;
+let calculoIR = 0;
+let valorDaParcela = 0;
+
+if (salarioIR <= 1903.98) {
+  calculoIR = 0;
+  valorDaParcela = 0;
+} else if (salarioIR >= 1903.99 && salarioIR <= 2826.65) {
+  aliquotaIR = 0.075;
+  valorDaParcela = 142.8;
+  // console.log("aliquotaIR:", aliquotaIR);
+} else if (salarioIR >= 2826.66 && salarioIR <= 3751.05) {
+  aliquotaIR = 0.15;
+  valorDaParcela = 354.8;
+  // console.log("aliquotaIR:", aliquotaIR);
+} else if (salarioIR >= 3751.06 && salarioIR <= 4664.68) {
+  aliquotaIR = 0.225;
+  valorDaParcela = 636.13;
+  // console.log("aliquotaIR:", aliquotaIR);
+} else {
+  aliquotaIR = 0.275;
+  valorDaParcela = 869.36;
+  // console.log("aliquotaIR:", aliquotaIR);
+}
+
+let calculaAliquota = aliquotaIR * salarioIR;
+let calculaParcela = calculaAliquota - valorDaParcela;
+let salarioLiquido = salarioIR - calculaParcela;
+
+console.log("salarioLiquido:", salarioLiquido);
 console.log("impostoINSS:", impostoINSS);
-// console.log("salarioLiquido", salarioLiquido);
+console.log("calculaAliquota:", calculaAliquota);
+console.log("valorDaParcela:", valorDaParcela);
