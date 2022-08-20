aadfc6a212b69cef7ce44a205a5de5f18feb33bb
uelionobre <uelionobre@hotmail.com>
2022-08-20 11:30:57 -0300

    continuo estudando o algoritmo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
@@ -2,11 +2,28 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let index = 0;
 let subIndex = 0;
 
-// Exercicio Bonus 1
+// Exercicio Bonus - Ordem crescente
 // Percorre n-1 vezes
 for (index = 1; index < numbers.length; index += 1) {
   for (subIndex = 0; subIndex < index; subIndex += 1) {
-    console.log("index:", index, "subIndex: ", subIndex);
+    // console.log("index:", index, "subIndex: ", subIndex);
+    let positionA = numbers[index];
+    let positionB = numbers[subIndex];
+
+    if (positionA < positionB) {
+      let position = numbers[index];
+      numbers[index] = positionB;
+      numbers[subIndex] = positionA;
+    }
+  }
+}
+
+// console.log("Ordena crescente: ", numbers);
+
+// Exercicio Bonus - Ordem decrescente
+for (index = 1; index < numbers.length; index += 1) {
+  for (subIndex = 0; subIndex < index; subIndex += 1) {
+    // console.log("index:", index, "subIndex: ", subIndex);
     let positionA = numbers[index];
     let positionB = numbers[subIndex];
 
@@ -17,4 +34,18 @@ for (index = 1; index < numbers.length; index += 1) {
     }
   }
 }
-console.log(numbers);
+// console.log("Ordena decrescente: ", numbers);
+
+// Exercicio Bonus - Multiplica pelo correspondente
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Reseta o array
+const multiplicados = numbers;
+
+for (index = 1; index < numbers.length; index += 1) {
+  for (subIndex = 0; subIndex < index; subIndex += 1) {
+    let position = numbers[index];
+    multiplicados[subIndex] = position;
+    multiplicados[index] = numbers[subIndex];
+  }
+}
+console.log("multiplicados: ", multiplicados);
+// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
