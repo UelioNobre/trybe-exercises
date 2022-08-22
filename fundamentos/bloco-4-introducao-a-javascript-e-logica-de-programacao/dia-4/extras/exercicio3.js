95ef6825babf70426ca058430b3db163948bb0a9
uelionobre <uelionobre@hotmail.com>
2022-08-22 19:16:44 -0300

    refatora exercicio para torna mais legível

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4/extras/exercicio3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4/extras/exercicio3.js
@@ -6,9 +6,14 @@
 
 let msgWins = 'A Ties';
 let play1 = 'tesoura';
-let play2 = 'pedra';
+let play2 = 'papel';
 
-let play1Vence = (play1 === 'papel' && play2 === 'pedra') ||  (play1 === 'tesoura' && play2 === 'papel') || (play1 === 'pedra' && play2 === 'tesoura')
-if (play1 !== play2) (play1Vence) ? msgWins = 'Player 1 won' : msgWins = 'Player 2 won';
+let play1Vence =
+  (play1 === 'papel' && play2 === 'pedra') ||
+  (play1 === 'tesoura' && play2 === 'papel') ||
+  (play1 === 'pedra' && play2 === 'tesoura')
+  
+if (play1 !== play2)
+  (play1Vence) ? msgWins = 'Player 1 won' : msgWins = 'Player 2 won';
 
 console.log(msgWins)
