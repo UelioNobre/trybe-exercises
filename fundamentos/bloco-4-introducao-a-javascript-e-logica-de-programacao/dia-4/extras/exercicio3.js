cdec5676f741543178b18f1b12ccb27472b0de09
uelionobre <uelionobre@hotmail.com>
2022-08-22 19:05:27 -0300

    refatora exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4/extras/exercicio3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4/extras/exercicio3.js
@@ -4,18 +4,11 @@
 // imprima o resultado no formato:
 // "Player 1 won" ou "A Ties" ou "Player 2 won".
 
-let papel = 2, tesoura = 3, pedra = 4;
-let msgP1Win = 'Player 1 won', msgP2Win = 'Player 2 won';
+let msgWins = 'A Ties';
+let play1 = 'tesoura';
+let play2 = 'pedra';
 
-let ganhador = 'A Ties';
-let play1 = tesoura;
-let play2 = pedra;
+let play1Vence = (play1 === 'papel' && play2 === 'pedra') ||  (play1 === 'tesoura' && play2 === 'papel') || (play1 === 'pedra' && play2 === 'tesoura')
+if (play1 !== play2) (play1Vence) ? msgWins = 'Player 1 won' : msgWins = 'Player 2 won';
 
-let play1Vence = (
-  (play1 === papel && play2 === pedra) || 
-  (play1 === tesoura && play2 === papel) ||
-  (play1 === pedra && play2 === tesoura)
-)
-
-if (play1 !== play2) ganhador = (play1Vence) ? msgP1Win: msgP2Win
-console.log(ganhador)
+console.log(msgWins)
