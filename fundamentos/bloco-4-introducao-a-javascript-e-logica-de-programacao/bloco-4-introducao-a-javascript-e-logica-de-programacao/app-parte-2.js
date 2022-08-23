72432e0c424e1cfcb2c77a20b38c43e6e95e2eff
uelionobre <uelionobre@hotmail.com>
2022-08-23 19:51:25 -0300

    Resolve item 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/app-parte-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/app-parte-2.js
@@ -8,3 +8,13 @@ function palindromo(palavra) {
 console.log(palindromo('arara'))
 console.log(palindromo('desenvolvimento'))
 console.log()
+
+// 2
+function maiorValor(array) {
+  let menor = array[0];
+  for (let valor of array) {
+    if (menor < valor) menor = valor;
+  }
+  return array.indexOf(menor);
+}
+console.log(maiorValor([2, 3, 6, 7, 10, 1]));
