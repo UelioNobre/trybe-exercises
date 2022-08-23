20de4b010b18cb17a08e0cae2907cf922c76a28e
uelionobre <uelionobre@hotmail.com>
2022-08-23 00:02:22 -0300

    bonus 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus.js
@@ -0,0 +1,21 @@
+
+let n = 5
+let saida = "";
+
+for (let index = 0; index < n; index++) {
+  for (let index2 = 0; index2 < n; index2++) { 
+    saida += '*'
+  }
+  saida += '\n'
+}
+
+console.log(saida)
+
+for (let index = 0; index < n; index++) {
+  for (let index2 = 0; index2 < n; index2++) { 
+    saida += '*'
+  }
+  saida += '\n'
+}
+
+console.log(saida)
