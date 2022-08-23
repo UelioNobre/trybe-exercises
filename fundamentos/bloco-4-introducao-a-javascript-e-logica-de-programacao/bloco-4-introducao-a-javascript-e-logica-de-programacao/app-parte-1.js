35ddc219af787dbee63a114c9ae9548bfe20e25c
uelionobre <uelionobre@hotmail.com>
2022-08-23 18:46:18 -0300

    Resolve item 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/app-parte-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/app-parte-1.js
@@ -34,3 +34,22 @@ console.log(`${info.personagem} e ${info2.personagem}`)
 console.log(`${info.origem} e ${info2.origem}`)
 console.log(`${info.nota} e ${info2.nota}`)
 console.log((info.recorrente == 'Sim' && info2.recorrente == 'Sim') ? 'Ambos recorrentes' : '')
+
+// 6
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+let nome = leitor.nome  + ' ' + leitor.sobrenome
+let livroFavorito = leitor.livrosFavoritos[0].titulo
+
+console.log(`O livro favorito de ${nome} se chama '${livroFavorito}'`)
