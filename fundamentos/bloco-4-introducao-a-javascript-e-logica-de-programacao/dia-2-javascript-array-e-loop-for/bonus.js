45f730725051914e58d73de172e3486c4c8a356e
uelionobre <uelionobre@hotmail.com>
2022-08-22 22:24:52 -0300

    Resolve ordenacao em crescente e decrescente

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
@@ -1,51 +1,27 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let index = 0;
-let subIndex = 0;
+console.log(numbers.join(', '))
 
-// Exercicio Bonus - Ordem crescente
-// Percorre n-1 vezes
-for (index = 1; index < numbers.length; index += 1) {
-  for (subIndex = 0; subIndex < index; subIndex += 1) {
-    // console.log("index:", index, "subIndex: ", subIndex);
-    let positionA = numbers[index];
-    let positionB = numbers[subIndex];
-
-    if (positionA < positionB) {
-      let position = numbers[index];
-      numbers[index] = positionB;
-      numbers[subIndex] = positionA;
+// Ordem decrescente
+for (let i = 1; i < numbers.length; i += 1){
+  for (let j = 0; j < i; j += 1){
+    if (numbers[i] > numbers[j]) {
+      let trocar = numbers[i];
+      numbers[i] = numbers[j]
+      numbers[j] = trocar
     }
   }
 }
+console.log('Ordem decrescente:', numbers.join(', '))
 
-// console.log("Ordena crescente: ", numbers);
-
-// Exercicio Bonus - Ordem decrescente
-for (index = 1; index < numbers.length; index += 1) {
-  for (subIndex = 0; subIndex < index; subIndex += 1) {
-    // console.log("index:", index, "subIndex: ", subIndex);
-    let positionA = numbers[index];
-    let positionB = numbers[subIndex];
-
-    if (positionA > positionB) {
-      let position = numbers[index];
-      numbers[index] = positionB;
-      numbers[subIndex] = positionA;
+// Ordena crescente
+for (let i = 1; i < numbers.length; i += 1){
+  for (let j = 0; j < i; j += 1){
+    if (numbers[i] < numbers[j]) {
+      let trocar = numbers[i];
+      numbers[i] = numbers[j]
+      numbers[j] = trocar
     }
   }
 }
-// console.log("Ordena decrescente: ", numbers);
+console.log('Ordem crescente:', numbers.join(', '))
 
-// Exercicio Bonus - Multiplica pelo correspondente
-numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Reseta o array
-const multiplicados = numbers;
-
-for (index = 1; index < numbers.length; index += 1) {
-  for (subIndex = 0; subIndex < index; subIndex += 1) {
-    let position = numbers[index];
-    multiplicados[subIndex] = position;
-    multiplicados[index] = numbers[subIndex];
-  }
-}
-console.log("multiplicados: ", multiplicados);
-// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
