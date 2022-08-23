e9dcbcee38c260668eb31f88a32d7563ce06011a
uelionobre <uelionobre@hotmail.com>
2022-08-23 19:53:39 -0300

    Resolve item 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/app-parte-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/app-parte-2.js
@@ -11,10 +11,20 @@ console.log()
 
 // 2
 function maiorValor(array) {
+  let maior = array[0];
+  for (let valor of array) {
+    if (maior < valor) maior = valor;
+  }
+  return array.indexOf(maior);
+}
+console.log(maiorValor([2, 3, 6, 7, 10, 1]));
+
+// 3
+function menorValor(array) {
   let menor = array[0];
   for (let valor of array) {
-    if (menor < valor) menor = valor;
+    if (menor > valor) menor = valor;
   }
   return array.indexOf(menor);
 }
-console.log(maiorValor([2, 3, 6, 7, 10, 1]));
+console.log(menorValor([2, 4, 6, 7, 10, 0, -3]));
