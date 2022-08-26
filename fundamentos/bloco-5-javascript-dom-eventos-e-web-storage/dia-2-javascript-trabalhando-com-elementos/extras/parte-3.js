a894eab208fb2e13de9bc6d2644e0247a9ad30ea
uelionobre <uelionobre@hotmail.com>
2022-08-26 18:38:14 -0300

    organiza arquivos

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/extras/parte-3.js
@@ -0,0 +1,21 @@
+// Remova todos os elementos filhos de paiDoPai exceto:
+// - pai
+// - primeiroFilhoDoFilho
+// - elementoOndeVoceEsta
+
+
+// let pai = document.getElementById('pai')
+
+// let terceiroFilho = document.getElementById('terceiroFilho');
+// let quartoFilho = document.getElementById('quartoEUltimoFilho');
+
+// pai.removeChild(terceiroFilho);
+// pai.removeChild(quartoFilho);
+
+let pai = document.getElementById('paiDoPai')
+
+// let terceiroFilho = document.getElementById('terceiroFilho');
+// let quartoFilho = document.getElementById('quartoEUltimoFilho');
+
+// pai.removeChild(terceiroFilho);
+// pai.removeChild(quartoFilho);
