4332a667326d7e7a56cba5a9aa4f35b8f79345a6
uelionobre <uelionobre@hotmail.com>
2022-08-26 15:43:34 -0300

    remove codigo de remocao de filhos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/parte-3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/parte-3.js
@@ -9,12 +9,5 @@ let pai = document.getElementById('pai')
 let terceiroFilho = document.getElementById('terceiroFilho');
 let quartoFilho = document.getElementById('quartoEUltimoFilho');
 
-pai.removeChild(terceiroFilho)
-pai.removeChild(quartoFilho)
-
-let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
-let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
-let segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
-
-elementoOndeVoceEsta.removeChild(primeiroFilhoDoFilho)
-elementoOndeVoceEsta.removeChild(segundoEUltimoFilhoDoFilho)
+pai.removeChild(terceiroFilho);
+pai.removeChild(quartoFilho);
