b4c2753510be6e971bb37da95c5583ed3f20a4cb
uelionobre <uelionobre@hotmail.com>
2022-08-30 18:53:25 -0300

    adiciona cor aos paragrafos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,8 +1,19 @@
 const selectBackground = document.querySelector('#backGround');
+const textColor = document.querySelector('#textColor');
+
 selectBackground.addEventListener('change', changeBackground)
-console.log(selectBackground)
+textColor.addEventListener('change', changeTextColor)
 
 // 1
 function changeBackground(e) {
   document.querySelector('body').style.backgroundColor = e.target.value;
+}
+
+// 2
+function changeTextColor(e) {
+  const tagsP = document.getElementsByTagName('p');
+  for (let tagP of tagsP) {
+    tagP.style.color = e.target.value;
+  }
+  console.log(tagsP)
 }
