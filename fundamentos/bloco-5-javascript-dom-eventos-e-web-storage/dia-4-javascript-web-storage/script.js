e1859a5ede4cb87cb1dd873334f53c5b3558cd37
uelionobre <uelionobre@hotmail.com>
2022-08-30 18:57:59 -0300

    adiciona controle de tamanho de texto

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,8 +1,13 @@
 const selectBackground = document.querySelector('#backGround');
 const textColor = document.querySelector('#textColor');
+const textSize = document.querySelector('#textSize');
+
 
 selectBackground.addEventListener('change', changeBackground)
 textColor.addEventListener('change', changeTextColor)
+textSize.addEventListener('change', changeTextSize)
+
+console.log(selectBackground)
 
 // 1
 function changeBackground(e) {
@@ -14,6 +19,15 @@ function changeTextColor(e) {
   const tagsP = document.getElementsByTagName('p');
   for (let tagP of tagsP) {
     tagP.style.color = e.target.value;
+  }
+  console.log(tagsP)
+}
+
+// 3
+function changeTextSize(e) {
+  const tagsP = document.getElementsByTagName('p');
+  for (let tagP of tagsP) {
+    tagP.style.fontSize = e.target.value + 'px';
   }
   console.log(tagsP)
 }
