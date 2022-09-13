4bf26a611bc501b3574f783fe5f64ad9811b9612
uelionobre <uelionobre@hotmail.com>
2022-09-13 14:31:36 -0300

    adcicao de arquivos de outroos dias

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/extras/parte-3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/extras/parte-3.js
@@ -12,10 +12,41 @@
 // pai.removeChild(terceiroFilho);
 // pai.removeChild(quartoFilho);
 
-let pai = document.getElementById('paiDoPai')
 
 // let terceiroFilho = document.getElementById('terceiroFilho');
 // let quartoFilho = document.getElementById('quartoEUltimoFilho');
 
 // pai.removeChild(terceiroFilho);
 // pai.removeChild(quartoFilho);
+
+// console.log(pai.childNodes);
+
+let pai = document.getElementById('pai');
+const todosOsFilhos = pai.children;
+console.log(todosOsFilhos.length);
+
+function remover_incrementando() {
+  for (let index = 0; index < todosOsFilhos.length; index += 1) {
+    const filhoAtual = todosOsFilhos[index];
+
+    if (filhoAtual.id !== 'elementoOndeVoceEsta'){
+      filhoAtual.remove();
+    }
+    
+    // Mostra o estado atual de todosOsFilhos
+    console.log(index, todosOsFilhos);
+  }
+}
+
+function remover_decrementando() {
+  for (let index = todosOsFilhos.length - 1; index >= 0; index -= 1) {
+    const filhoAtual = todosOsFilhos[index];
+    
+    if (filhoAtual.id !== 'elementoOndeVoceEsta') {
+      filhoAtual.remove();
+    }
+    
+    // Mostra o estado atual de todosOsFilhos
+    console.log(index, todosOsFilhos);
+  }
+}
