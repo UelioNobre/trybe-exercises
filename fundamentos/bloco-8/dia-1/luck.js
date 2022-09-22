d5fd49991ba0894fe67468bb7513ca0fb655320e
uelionobre <uelionobre@hotmail.com>
2022-09-22 15:52:51 -0300

    Implementa mais uma funcao de ordem superior em results

--- fundamentos/bloco-8/dia-1/luck.js
+++ fundamentos/bloco-8/dia-1/luck.js
@@ -1,6 +1,6 @@
 const sortition = () => parseInt(Math.random() * 5 + 1, 10);
 const checker = (number, game) => number === game();
 const message = (win) => (win === true) ? 'Parabéns você ganhou' : 'Tente novamente';
-const results = (checker) => checker(4, sortition) === true ? message(true) : message(false);
+const results = (checker, show) => checker(4, sortition) === true ? show(true) : show(false);
 
-console.log(results(checker));
+console.log(results(checker, message));
