98af3177cc604e9875b6ce39173392f97bf0dfd7
uelionobre <uelionobre@hotmail.com>
2022-09-23 11:54:36 -0300

    template basico.

---
+++ fundamentos/bloco-8/dia-1/hasName.js
@@ -0,0 +1,9 @@
+// Um exercício para procurar um nome utilizando a HoF some.
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+};
+
+console.log(hasName(names, 'Ana'));
