42bed6e35e558602d26f62f9c04a1e807a0d24e3
uelionobre <uelionobre@hotmail.com>
2022-09-27 11:20:24 -0300

    Adiciona testes para a função nameAndAge

--- fundamentos/bloco-8/dia-3/tests/exercise1.spec.js
+++ fundamentos/bloco-8/dia-3/tests/exercise1.spec.js
@@ -21,18 +21,39 @@ describe('Testa formatedBookName', () => {
   it('Verica se formatedBookName retorna um array"', () => {
     expect(Array.isArray(formatedBookNames())).toBeTruthy();
   });
-
+  
   it('Verica se o tamanho de formatedBookName é maior que zero', () => {
     expect(formatedBookNames().length).toBeGreaterThan(0);
   });
-
+  
   it('Verica se formatedBookName retorna um array de string', () => {
     formatedBookNames().forEach((item) => expect(typeof item).toBe('string'));
   });
-
+  
   it('Verica se formatedBookName retorna o nome do livro, genero e nome do autor', () => {
     expect(formatedBookNames()).toEqual(expect.arrayContaining(expectedResult));
   });
 });
 
 // NAME AND AGE
+describe('Testa a função nameAndAge', () => { 
+  it('Verica se nameAndAge é uma função', () => {
+    expect(typeof nameAndAge).toBe('function');
+  });
+  
+  it('Verica se nameAndAge retorna um valor diferente de "undefined"', () => {
+    expect(nameAndAge()).not.toBeUndefined();
+  });
+  
+  it('Verica se nameAndAge é um array', () => {
+    expect(Array.isArray(nameAndAge())).toBeTruthy();
+  });
+  
+  it('Verica se o tamanho de nameAndAge é maior que zero', () => {
+    expect(nameAndAge().length).toBeGreaterThan(0);
+  });
+  
+  it('Verica se nameAndAge um array de objetos', () => {
+    nameAndAge().forEach((item) => expect(typeof item).toBe('object'));
+  });
+});
