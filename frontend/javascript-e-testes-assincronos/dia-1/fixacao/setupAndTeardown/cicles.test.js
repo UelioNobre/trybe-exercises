da3e3ff59153018963cbd659a6234f2131eb1a9e
uelionobre <uelionobre@hotmail.com>
2022-10-07 12:43:26 -0300

    Organiza o TearDown em cada describe

--- frontend/javascript-e-testes-assincronos/dia-1/fixacao/setupAndTeardown/cicles.test.js
+++ frontend/javascript-e-testes-assincronos/dia-1/fixacao/setupAndTeardown/cicles.test.js
@@ -10,32 +10,61 @@ const removeCity = (city) => {
   cities = cities.filter((eachCity) => eachCity !== city);
 };
 
-// Setup
-beforeEach(() => {
-  cities = [...cities, 'Pindamonhangaba'];
+describe('Agrupa o primeiro bloco de testes', () => {
+  beforeEach(() => {
+    cities = [...cities, 'Pindamonhangaba'];
+  });
+
+  afterEach(() => {
+    cities = [];
+  });
+
+  test('Testa a função addCity dentro do primeiro bloco de testes', () => {
+    expect.assertions(5);
+    addCity('Campinas');
+    addCity('Goiania');
+    addCity('Recife');
+    expect(cities).toHaveLength(4);
+    expect(cities).toContain('Campinas');
+    expect(cities).toContain('Goiania');
+    expect(cities).toContain('Recife');
+    expect(cities).toContain('Pindamonhangaba');
+  });
+
+  test('Testa a função removeCity dentro do primeiro bloco de testes', () => {
+    expect.assertions(2);
+    removeCity('Pindamonhangaba');
+    expect(cities).toHaveLength(0);
+    expect(cities).not.toContain('Pindamonhangaba');
+  });
 });
 
-afterEach(() => {
-  cities = [];
-});
+describe('Agrupa o segundo bloco de testes', () => {
+  beforeEach(() => {
+    cities = [...cities, 'Tangamandapio'];
+  });
 
-// Testes
-test('Testa a função addCity', () => {
-  expect.assertions(4);
-  addCity('Campinas');
-  addCity('Goiania');
-  addCity('Recife');
-  expect(cities).toHaveLength(3);
-  expect(cities).toContain('Campinas');
-  expect(cities).toContain('Goiania');
-  expect(cities).toContain('Recife');
-});
+  afterEach(() => {
+    cities = [];
+  });
 
-test('Testa a função removeCity', () => {
-  expect.assertions(4);
-  removeCity('Campinas');
-  expect(cities).toHaveLength(2);
-  expect(cities).not.toContain('Campinas');
-  expect(cities).toContain('Goiania');
-  expect(cities).toContain('Recife');
+  test('Testa a função addCity dentro do segundo bloco de testes', () => {
+    expect.assertions(5);
+    addCity('Campinas');
+    addCity('Goiania');
+    addCity('Recife');
+    expect(cities).toHaveLength(4);
+    expect(cities).toContain('Campinas');
+    expect(cities).toContain('Goiania');
+    expect(cities).toContain('Recife');
+    expect(cities).toContain('Tangamandapio');
+  });
+
+  test('Testa a função removeCity dentro do segundo bloco de testes', () => {
+    expect.assertions(3);
+    removeCity('Tangamandapio');
+    expect(cities).toHaveLength(0);
+    expect(cities).not.toContain('Tangamandapio');
+    expect(cities).not.toContain('Pindamonhangaba');
+  });
 });
