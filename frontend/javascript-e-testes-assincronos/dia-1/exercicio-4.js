a238dde994270c8f561b325b37559fc7e3dca257
uelionobre <uelionobre@hotmail.com>
2022-10-07 21:20:55 -0300

    Adiciona callbacks a sendMarsTemperature

--- frontend/javascript-e-testes-assincronos/dia-1/exercicio-4.js
+++ frontend/javascript-e-testes-assincronos/dia-1/exercicio-4.js
@@ -5,12 +5,26 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
 // crie a função sendMarsTemperature abaixo
-const sendMarsTemperature = () => {
-    const temperature = getMarsTemperature();
-    const msg = `A temperatura de Marte é: ${temperature} graus celsius`;
-    console.log(msg);
+const sendMarsTemperature = (callback) => {
+  const temperature = getMarsTemperature();
+  const msg = `A temperatura de Marte é: ${temperature} graus celsius`;
+  callback(temperature);
+  // console.log(msg);
 }
 
 // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
-setTimeout(() => sendMarsTemperature(), messageDelay()); 
+setTimeout(() => {
+  // Definição da função sendMarsTemperature...
+  sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+  sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
+}, messageDelay());
+
