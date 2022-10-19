8102328cfb926368094425fdfcb5b313f142a56e
uelionobre <uelionobre@hotmail.com>
2022-10-19 16:01:54 -0300

    testes do exercicio

--- frontend/javascript-e-testes-assincronos/dia-1/exercicio-5.test.js
+++ frontend/javascript-e-testes-assincronos/dia-1/exercicio-5.test.js
@@ -11,26 +11,26 @@ describe('Testa a função uppercase', (done) => {
 
   // 1. Criamos o it
   // O parametro "done()", sinaliza que o teste deve ser terminado.
-  // Caso remova o donw, um erro do tipo "timeout" é lançado.
+  // Caso remova o donw, um erro do tipo "Exceeded timeout of 5000 ms for a test." é lançado.
   it('uppercase "test" to equal "TEST"', (done) => {
-    
+
     // Who is "done"?
     // here: https://jestjs.io/docs/asynchronous#callbacks
-    
+
     const callBack = (strUpperCase) => {
       try {
         expect(strUpperCase).toBe(expected);
-        
+
         // 5. Chamamos a função `done` do Jest
         done();
       } catch (error) {
-        
+
         // 6. Chamamos a função `done` com o parâmetro `error`
         done(error);
       }
     };
-    
-    const expected = "FRANSUELIO NA TRYBE";
-    uppercase('Fransuelio na trybe', callBack);
+
+    const expected = 'VAMO COM TUDO!';
+    uppercase('vamo com tudo!', callBack);
   });
 });
